1b6e6305a7da4a2e9b718d8b3099d53d03d6d8cd
Racio77 <rachel.mshomi@africaehs.org>
2020-06-28 16:33:16 +0300

    added image functionality in request plan form

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,100 +13,100 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: Column(
         children: <Widget>[
-    Expanded(
-      flex: 3,
-      child: Stack(
-        children: <Widget>[
-          OpaqueImage(
-            imageUrl: "assets/images/signup-avator.jpg",
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Dashboard", textAlign: TextAlign.left, 
-                    style: TextStyle(fontSize: 22.0, color: Colors.white, )
+      Expanded(
+        flex: 3,
+        child: Stack(
+          children: <Widget>[
+            OpaqueImage(
+              imageUrl: "assets/images/signup-avator.jpg",
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Dashboard", textAlign: TextAlign.left, 
+                      style: TextStyle(fontSize: 22.0, color: Colors.white, )
+                      ),
                     ),
-                  ),
-                  MyInfo(),
-                ],
-              ),
-              ),
-          ),
-        ],
-      ),
-    ),
-    Expanded(
-      flex: 4,
-      child: SingleChildScrollView(
-              child: Column(
-                children: <Widget> [
-                  SizedBox(height: 10.0),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: (){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => RequestPlanPage(
-                              )
-                              )
-                              );
-                            },
-                            child: DashboardInfoCard(
-                            //imagePath: "assets/images/request-plan.png",
-                            imagePath: Icons.receipt,
-                            firstName: "Request",
-                            secondName: "Insurance Plan",
-                            ),
-                          ),
-                        SizedBox(width: 10.0),
-                          GestureDetector(
-                            onTap: (){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => CurrentPlanPage()));
-                            },
-                            child: DashboardInfoCard(
-                            //imagePath: "assets/images/current-plan.png",
-                            imagePath: Icons.cached,
-                            firstName: "Review",
-                            secondName: "Current Plan",
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: (){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => PastPlanPage()));
-                            },
-                            child: DashboardInfoCard(
-                            //imagePath: "assets/images/request-plan.png",
-                            imagePath: Icons.beenhere,
-                            firstName: "View Past",
-                            secondName: "Insurance Plans",
-                            ),
-                          ),
-                        SizedBox(width: 10.0),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
+                    MyInfo(),
+                  ],
+                ),
+                ),
+            ),
           ],
-          ),
+        ),
       ),
-    ),
-        ],
-    ),
-      );
+      Expanded(
+        flex: 4,
+        child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget> [
+                    SizedBox(height: 10.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => RequestPlanPage(
+                                )
+                                )
+                                );
+                              },
+                              child: DashboardInfoCard(
+                              //imagePath: "assets/images/request-plan.png",
+                              imagePath: Icons.receipt,
+                              firstName: "Request",
+                              secondName: "Insurance Plan",
+                              ),
+                            ),
+                          SizedBox(width: 10.0),
+                            GestureDetector(
+                              onTap: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => CurrentPlanPage()));
+                              },
+                              child: DashboardInfoCard(
+                              //imagePath: "assets/images/current-plan.png",
+                              imagePath: Icons.cached,
+                              firstName: "Review",
+                              secondName: "Current Plan",
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => PastPlanPage()));
+                              },
+                              child: DashboardInfoCard(
+                              //imagePath: "assets/images/request-plan.png",
+                              imagePath: Icons.beenhere,
+                              firstName: "View Past",
+                              secondName: "Insurance Plans",
+                              ),
+                            ),
+                          SizedBox(width: 10.0),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+            ],
+            ),
+        ),
+      ),
+          ],
+      ),
+        );
   }
 }
--- lib/src/pages/request_plan_page.dart
+++ lib/src/pages/request_plan_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class RequestPlanPage extends StatefulWidget{
+  
   @override
-  _RequestPlanPageState createState() => _RequestPlanPageState();
+  RequestPlanPageState createState() => RequestPlanPageState();
 }
 
-class _RequestPlanPageState extends State<RequestPlanPage>{
+class RequestPlanPageState extends State{
 
 Widget _buildVehicleYearField(){
   return TextFormField(
@@ -55,6 +58,26 @@ Widget _buildPlateNumberField(){
 int insurancePlanGroup = 1;
 int ownershipGroup = 1;
 int licenseGroup = 1;
+
+File imageURI;
+
+  Future getImageFromCamera() async {
+
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      imageURI = image;
+    });
+  }
+
+  Future getImageFromGallery() async {
+
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      imageURI = image;
+    });
+  }
 
   @override
   Widget build(BuildContext context){
@@ -277,7 +300,7 @@ int licenseGroup = 1;
           Text("What is your vehicle's make?", 
           softWrap: true, style: TextStyle(fontSize: 16.0),
           ),
-          SizedBox(height: 10.0),
+           SizedBox(height: 10.0),
           _buildVehicleMakeField(),
           SizedBox(height: 10.0),
           Text("What is your vehicle's model?", 
@@ -299,23 +322,62 @@ int licenseGroup = 1;
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Container(
-              height: 220,    
+              width: 150.0,
+              height: 200.0,    
               color: Colors.black12,
-              child: Icon(Icons.add, size: 35, color: Colors.black38,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  imageURI == null
+                  ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.camera_alt, size: 40, color: Colors.black38,), 
+                          onPressed: () => getImageFromCamera(),
+                        ),
+                        SizedBox(height: 10),
+                        GestureDetector(
+                          onTap: () => getImageFromGallery(),
+                          child: Text("or select an image from the gallery", style: TextStyle(fontStyle: FontStyle.italic),)),
+                    ],
+                  )
+                  : Image.file(imageURI, width: 300, height: 200, fit: BoxFit.cover),  
+                ],
+              ),
             ),
           ),
+          SizedBox(height: 10.0),
+          imageURI != null
+          ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Change image?", style: TextStyle(fontStyle: FontStyle.italic),),
+              SizedBox(width: 10),
+              IconButton(
+                icon: Icon(Icons.add_a_photo, color: Colors.black38,), 
+                onPressed: () => getImageFromCamera(),
+                        ),
+              SizedBox(width: 10),
+              IconButton(
+                icon: Icon(Icons.add_photo_alternate, color: Colors.black38,), 
+                onPressed: () => getImageFromGallery(),
+                        ),
+            ],
+          )
+          : Text(" "),
           SizedBox(height: 20.0),
           Container(
-            height: 50.0,
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Center(
-              child: Text("Submit Application", style: TextStyle(fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.bold)),
-            ),
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: RaisedButton(
+          onPressed: () {},
+          child: Text("Submit Application", style: TextStyle(fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.bold)),
+          textColor: Colors.white,
+          color: Theme.of(context).primaryColor,
+          padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+          )
           ),
-          SizedBox(height: 50.0),
+          SizedBox(height: 75.0),
         ],
       ),
     );
